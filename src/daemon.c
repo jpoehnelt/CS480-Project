c0e12a6cae515424f084c222e78be8a4ce883ff5
tz32 <tz32@nau.edu>
2015-04-09 10:49:27 -0700

    Comments on the c field

--- src/daemon.c
+++ src/daemon.c
@@ -1,9 +1,15 @@
-/**************************
+/* ---------------
+ Justin Poehnelt
+ Ian Humphrey
+ Tomas Zukowski
+ 
+ CS 480
+ Homework 7
+ 4/9/15
+ --------------- */
 
-***************************/
 #include "daemon.h"
 
-<<<<<<< Updated upstream
 void dameon_init() {
 	pid_t pid, sid;
 
@@ -18,27 +24,33 @@ void dameon_init() {
 		exit(0);
 	}
 
+    // If setting the signal ID failed, print an error
 	if (pid == 0) {
 		sid = setsid();
 		if (sid < 0) {
 			perror("setsid failed");
 			exit(1);
 		}
+        
+        // Otherwise print the number of the process
 		printf("Daemon PID: %d\n", getpid());
 	}
 
+    // Move the process into a common working directory
 	if(chdir("/tmp") < 0){
 		perror("Could not change directory.");
 		exit(1);
 	}
 
-	// umask(0);
+    // Grant all rights to allow the Daemon read and write access
+    umask(0);
 
 	// Close file descript
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
 
+    // Write the start of the process to the system log
 	setlogmask (LOG_UPTO (LOG_NOTICE));
 	openlog ("Daemon Webserver", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 	syslog (LOG_NOTICE, "Program started by User: %d, PID: %d", getuid(), getpid());
@@ -49,23 +61,8 @@ void dameon_init() {
 
 	return;
 }
-=======
 
-#include <signal.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <syslog.h>
-
-void dameon_signal(int sig_no);
-void dameon_exit();
-void runserver();
-
-
->>>>>>> Stashed changes
-
+// Gracefully terminate the daemon process when the signal is caught
 void dameon_exit(int sig_no) {
 	syslog (LOG_NOTICE, "Caught Signal: %d", sig_no);
 	syslog(LOG_NOTICE, "Program exiting");
--- src/main.c
+++ src/main.c
@@ -1,82 +1,13 @@
-
-
-
-<<<<<<< Updated upstream
-
 #include "main.h"
 int main() {
 	dameon_init();
 	
+    // Run the process code
+    // Wait for 5 seconds and print to the system log
 	while(1) {
 		syslog(LOG_NOTICE, "Running Server");
 		sleep(5);
 	}
 
 	return 1;
-=======
-#include <signal.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <syslog.h>
-
-
-void dameon_run();
-void runserver();
-
-int main() {
-    dameon_run();
-    
-    return EXIT_SUCCESS;
-}
-
-void dameon_run() {
-    pid_t pid, sid;
-    
-    // Create child process
-    if ((pid = fork()) < 0) {
-        printf("Fork failure!\n");
-        exit(1);
-    }
-    
-    // Kill parent
-    if (pid > 0) {
-        exit(0);
-    }
-    
-    if (pid == 0) {
-        sid = setsid();
-        if (sid < 0) {
-            perror("setsid failed");
-            exit(1);
-        }
-        printf("Daemon PID: %d\n", getpid());
-    }
-    
-    if(chdir("/tmp") < 0){
-        perror("Could not change directory.");
-        exit(1);
-    }
-    
-    // umask(0);
-    
-    // Close file descript
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-    
-    setlogmask (LOG_UPTO (LOG_NOTICE));
-    openlog ("Daemon Webserver", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-    syslog (LOG_NOTICE, "Program started by User: %d, PID: %d", getuid(), getpid());
-    
-    
-    // Catch Signals
-    signal(SIGUSR1, dameon_signal);
-    
-    runserver();
-    
-    return;
->>>>>>> Stashed changes
 }
