a3239d2008787969bc3afbc16748d9a2f68df256
tz32 <tz32@nau.edu>
2015-04-16 10:42:41 -0700

    Added address declarations in client

--- src/client.c
+++ src/client.c
@@ -2,5 +2,18 @@
 
 int main() {
 	
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    client_address.sin_family = AF_INET;
+    client_address.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    client_address.sin_port = htons(PORT);
+    
+    if(connect(client_socket, (struct sockaddr *) &client_address, sizeof(client_address)) == -1)
+    {
+        // log error if there are issues creating a connection
+    }
+    
+    // client loop
+    
 	return 0;
 }
