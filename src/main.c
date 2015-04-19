b832b99bb56aa0e3773224448bbcc5bfa6d75d85
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-04-19 11:15:20 -0700

    fixed log messages and failed attempt at handle client

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ void runserver() {
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) 
     {
-        printf("Failed to create server socket");
+        syslog (LOG_NOTICE,"Failed to create server socket");
         exit(1);
     }
 
@@ -37,12 +37,14 @@ void runserver() {
 	// Bind Port to Socket
     if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) 
     {
-        printf("Failed to bind server socket");
+        syslog (LOG_NOTICE, "Failed to bind server socket");
         exit(1);
     }
 
 	// Listen for Connections with Queue 
     listen(server_socket, NUM_CONNECTIONS);
+
+    syslog (LOG_NOTICE, "Waiting for connections on port: %d", PORT);
 
 	while (1) {
         int client_socket = accept(server_socket, NULL, NULL);
@@ -51,29 +53,33 @@ void runserver() {
         if(pthread_create(&thread, NULL, handle_client, (void*)&client_socket) != 0)
         {
             // log error if there was an issue creating new thread
-        }
-		// Accept Connection
-
-		// Do threading		
+        }			
 	}
 }
 
 void* handle_client(void* arg) {
     int client_socket = (int) arg;
-    int done = 0;
     char input;
+    syslog (LOG_NOTICE, "Thread created for socket.");
 
-    while (!done) {
-        read(client_socket, &input, 1);
-        
-        if (input == 'q')
-        {
-            write(client_socket, &input, sizeof(char));
-            break;
-        }
-        
-        write(client_socket, &input, sizeof(char));
-    }
-    // close
+    // while (1) {
+    //     read(client_socket, &input, 1);
+
+    //     syslog (LOG_NOTICE, "Getting input: %c", input);
+
+    //     if (input == 'q')
+    //     {
+    //         syslog (LOG_NOTICE, "Echoed char: q");
+    //         write(client_socket, &input, sizeof(char));
+    //         break;
+    //     }
+    //     syslog (LOG_NOTICE, "Echoed char.");
+    //     write(client_socket, &input, sizeof(char));
+    // }
+    
+    // close socket
+    syslog (LOG_NOTICE, "Closing connection");
+    close(client_socket);
+
     exit(0);
 }
