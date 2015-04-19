165acf389d113a3f18171c30dedfacff52e88c7f
tz32 <tz32@nau.edu>
2015-04-19 12:07:42 -0700

    attempt at client read and updated header

--- src/client.c
+++ src/client.c
@@ -11,8 +11,21 @@
 #include "client.h"
 
 int main() {
-	
+    
+    // arrays to the client and server messages
+    char message[1000] , server_reply[2000];
+    
+    // declare server socket and client address struct
+    int client_socket;
+    struct sockaddr_in client_address;
+    
+    // create the socket -- TCP socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1)
+    {
+        syslog (LOG_NOTICE,"Failed to create client socket");
+        exit(1);
+    }
     
     client_address.sin_family = AF_INET;
     client_address.sin_addr.s_addr = inet_addr(SERVER_ADDR);
@@ -20,11 +33,40 @@ int main() {
     
     if(connect(client_socket, (struct sockaddr *) &client_address, sizeof(client_address)) == -1)
     {
-        // log error if there are issues creating a connection
+        printf("Failed to establish connection between the client and server");
     }
     
-    // client loop
+    // Bind Port to Socket
+    if (bind(client_socket, (struct sockaddr*)&client_address, sizeof(client_address)) == -1)
+    {
+        syslog (LOG_NOTICE, "Failed to bind client socket");
+        exit(1);
+    }
     
     
-	return 0;
+    do
+    {
+        scanf("%s" , message);
+        
+        if( send(client_socket , message , strlen(message) , 0) < 0)
+        {
+            printf("Send failed");
+            return 1;
+        }
+        
+        //Receive a reply from the server
+        if( recv(client_socket , server_reply , 2000 , 0) < 0)
+        {
+            printf("recv failed");
+            break;
+        }
+        
+        printf("Server reply :");
+        printf(server_reply);
+    }
+    while (1); //message != 'q');
+    
+    return 0;
 }
+
+
--- src/client.h
+++ src/client.h
@@ -8,3 +8,18 @@
  4/19/15
  */
 // client.h
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define PORT 8989
+#define SERVER_ADDR "127.0.0.1"
